f48ecc98eeae0f51e252006358173527d83d5a29
Kirill Kravtsov <killreal777.itmo@gmail.com>
2024-11-29 22:04:45 +0300

    Add: DTO examples

--- backend/src/main/java/itmo/is/dto/domain/request/CreateChapterRequest.java
+++ backend/src/main/java/itmo/is/dto/domain/request/CreateChapterRequest.java
@@ -1,14 +1,19 @@
 package itmo.is.dto.domain.request;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import io.swagger.v3.oas.annotations.media.Schema;
 
 public record CreateChapterRequest(
+
+        @Schema(example = "Operation Systems Chapter")
         @JsonProperty(value = "name", required = true)
         String name,
 
+        @Schema(example = "Klimenkov's Legion")
         @JsonProperty(value = "parent_legion", required = false)
         String parentLegion,
 
+        @Schema(example = "false")
         @JsonProperty(value = "admin_edit_allowed", required = true)
         boolean adminEditAllowed
 ) {
--- backend/src/main/java/itmo/is/dto/domain/request/UpdateSpaceMarineRequest.java
+++ backend/src/main/java/itmo/is/dto/domain/request/UpdateSpaceMarineRequest.java
@@ -1,25 +1,32 @@
 package itmo.is.dto.domain.request;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import io.swagger.v3.oas.annotations.media.Schema;
 import itmo.is.dto.domain.CoordinatesDto;
 import itmo.is.model.domain.AstartesCategory;
 
 public record UpdateSpaceMarineRequest(
+
+        @Schema(example = "Alexey Volkov")
         @JsonProperty(value = "name", required = true)
         String name,
 
         @JsonProperty(value = "coordinates", required = true)
         CoordinatesDto coordinates,
 
+        @Schema(example = "90")
         @JsonProperty(value = "health", required = true)
         Double health,
 
+        @Schema(example = "false")
         @JsonProperty(value = "loyal", required = false)
         boolean loyal,
 
+        @Schema(example = "183")
         @JsonProperty(value = "height", required = true)
         Integer height,
 
+        @Schema(example = "TERMINATOR")
         @JsonProperty(value = "category", required = false)
         AstartesCategory category
 ) {
