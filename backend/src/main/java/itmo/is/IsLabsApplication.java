f74a5a513862c065b6733ee280b0769233b891b7
Kirill Kravtsov <killreal777.itmo@gmail.com>
2024-12-06 21:47:30 +0300

    Update: rename project

--- backend/src/main/java/itmo/is/IsLabsApplication.java
+++ backend/src/main/java/itmo/is/IsLabsApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class IsLab1Application {
+public class IsLabsApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(IsLab1Application.class, args);
+        SpringApplication.run(IsLabsApplication.class, args);
     }
 
 }
