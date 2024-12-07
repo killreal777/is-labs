4dee4985c3b65e2120f0ad3d1dac296353573e94
Kirill Kravtsov <killreal777.itmo@gmail.com>
2024-12-07 13:45:45 +0300

    Fix: exception message

--- src/main/java/itmo/is/exception/EntityNotFoundWithIdException.java
+++ src/main/java/itmo/is/exception/EntityNotFoundWithIdException.java
@@ -4,6 +4,6 @@ import jakarta.persistence.EntityNotFoundException;
 
 public class EntityNotFoundWithIdException extends EntityNotFoundException {
     public EntityNotFoundWithIdException(Class<?> entityClass, Long id) {
-        super(entityClass.getSimpleName() + "not found with id " + id);
+        super(entityClass.getSimpleName() + " not found with id " + id);
     }
 }
